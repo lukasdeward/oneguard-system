76438953fc09b8d4e6db3e294c7eda43f0d421e2
lukvxx <lukas@deward.de>
2025-05-03 11:32:38 +0200

    add webhook for shop registration

--- server/api/types/webhookTypes.ts
+++ server/api/types/webhookTypes.ts
@@ -7,3 +7,9 @@ export type createVerificationWebhook = {
     order_value: string;
     order_timestamp: string;
 }
+
+export type registerShopWebhook = {
+    shop: string;
+    access_token: string;
+    public_proxy: string;
+}
--- server/api/webhooks/registerShop.ts
+++ server/api/webhooks/registerShop.ts
@@ -1,9 +1,34 @@
+import { PrismaClient } from "@prisma/client";
 
 export default defineEventHandler(async (event) => {
     const body = await readBody(event);
     console.log('Received webhook:', body);
+    const prisma = new PrismaClient();
+
+    const register = await prisma.merchants.upsert({
+        where: {
+            shop: body.shop,
+        },
+        update: {
+            key: body.access_token,
+            public_proxy: body.public_proxy,
+        },
+        create: {
+            shop: body.shop,
+            key: body.access_token,
+            public_proxy: body.public_proxy,
+        },
+    });
+
+    if (!register) {
+        return {
+            connectionCreated: false,
+            message: 'A error occurred while registering the Shop',
+        }
+    }
+
     return {
         connectionCreated: true,
-        message: 'Webhook registered successfully',
+        message: 'Shop registered successfully',
     }
 });
